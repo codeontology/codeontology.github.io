f9964a5b80765927fdbeb19c848ce03ddeca0f30
Mattia Setzu <davine.research@gmail.com>
2016-03-07 16:34:36 +0000

    Added jekyll stubs

---
+++ public/js/query.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    // Listeners
+    $("#query").on("click", query);
+});
+
+var query = function(e) {
+
+};
---
+++ storage/framework/views/ec6cc2902617f200bdec92317660a0b67ee06df4.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+<title><?php echo $__env->yieldContent('title'); ?></title>
+<meta name="description" content="CodeOntology is an ontology and tool to generate datasets from Java repositories.">
+<meta name="keywords" content="Java CodeOntology analyser semantic web">
+<meta name="author" content="Mattia Setzu">
+
+<!-- <link rel="stylesheet" title="Dept-like" media="screen, projection" type="text/css" href="<?php echo URL::to('/public/css/bootstrap.min.css'); ?>" /> -->
+<link rel="icon" href="/favicon.ico" />
+</head>
+<body>
+    <?php echo $__env->yieldContent("content"); ?>
+</body>
+</html>
